93f128c3c26d910818967a8b74f0e0cd1e359155
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-06-16 20:34:25 +0600

    added a get route for all toys to show all the toys in website

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ async function run() {
       res.send(result)
     })
 
-    
+    app.get('/allToys', async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
